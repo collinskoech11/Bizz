bef85d82e7a77d438dffe6c5eb25eb92dc6588a4
Collins Koech <collinskoechck34@gmail.com>
2020-09-07 15:57:01 +0300

    added script.js

--- user-app/src/script.js
+++ user-app/src/script.js
@@ -1,6 +1,1 @@
-document.getElementById('active').addEventListener('click',
-function(){
-    const appheader = document.querySelector('App-header');
-    appheader.classList.toggle('script');
-    appheader.removeClass('script');
-})
+
