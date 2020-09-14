39c30992869e96f1bbeb56ee7203c4e584339eed
Collins Koech <collinskoechck34@gmail.com>
2020-09-14 04:02:02 +0300

    update

--- user-app/src/App.js
+++ user-app/src/App.js
@@ -1,16 +1,44 @@
 import React from 'react';
-import './App.css';
 import './script.js';
-import Hello from './sayHello';
+import { Carousel } from 'react-bootstrap';
 
 function App(){
-  const sayHello = () =>{
-    console.log('sasa');
-  }
   return(
-    <div>
-      <Hello/>
-    </div>
+    <Carousel>
+    <Carousel.Item interval={1000}>
+      <img
+        className="d-block w-100"
+        src="nature1.jpg"
+        alt="First slide"
+      />
+      <Carousel.Caption>
+        <h3>Sunset</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item interval={500}>
+      <img
+        className="d-block w-100"
+        src="nature2.jpg"
+        alt="Third slide"
+      />
+      <Carousel.Caption>
+        <h3>Green land</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src="nature3.jpg"
+        alt="Third slide"
+      />
+      <Carousel.Caption>
+        <h3>Hills</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+  </Carousel>
   );
 }
 export default App;
